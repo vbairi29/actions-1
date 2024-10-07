d2281dd7ebfbe191831de34919becbd02eb36c06
vbairi29 <74100920+vbairi29@users.noreply.github.com>
2024-10-07 23:10:16 +0530

    updated clor codes

--- ascii_script.sh
+++ ascii_script.sh
@@ -1,13 +1,13 @@
 # sudo apt-get update 
-echo "####### Instaling Cowsay package #######"
+echo "\e[5m;0;34 ####### Instaling Cowsay package #######"
 sudo apt-get install cowsay -y
-echo "####### Executing Cowsay CMD #######"
+echo "\e[5m;0;31 ####### Executing Cowsay CMD #######"
 cowsay -f dragon "I am a dragon from new city and a NEW DRAGON ...... Hee..heee...!!!" >> dragon.txt
-echo "####### Search dragon word  #######"
+echo "\e[5m;1;30 ####### Search dragon word  #######"
 grep -i "dragon" dragon.txt
-echo "####### List all files  #######"
+echo "\e[5m;0:33 ####### List all files  #######"
 ls -ltra
-echo "####### read dragon file  #######"
+echo "\e[5m;1;33 ####### read dragon file  #######"
 cat dragon.txt
-echo "####### Todays date  #######"
+echo "\e[5m;0:36 ####### Todays date  #######"
 date
