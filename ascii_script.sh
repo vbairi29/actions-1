319e8c352011c385bdd573a92754eec99a9bd82e
vbairi29 <74100920+vbairi29@users.noreply.github.com>
2024-10-07 23:11:36 +0530

    echo updated

--- ascii_script.sh
+++ ascii_script.sh
@@ -1,13 +1,13 @@
 # sudo apt-get update 
-echo "\e[5m;0;34 ####### Instaling Cowsay package #######"
+echo -e "\e[5m;0;34 ####### Instaling Cowsay package #######"
 sudo apt-get install cowsay -y
-echo "\e[5m;0;31 ####### Executing Cowsay CMD #######"
+echo -e "\e[5m;0;31 ####### Executing Cowsay CMD #######"
 cowsay -f dragon "I am a dragon from new city and a NEW DRAGON ...... Hee..heee...!!!" >> dragon.txt
-echo "\e[5m;1;30 ####### Search dragon word  #######"
+echo -e "\e[5m;1;30 ####### Search dragon word  #######"
 grep -i "dragon" dragon.txt
-echo "\e[5m;0:33 ####### List all files  #######"
+echo -e "\e[5m;0:33 ####### List all files  #######"
 ls -ltra
-echo "\e[5m;1;33 ####### read dragon file  #######"
+echo -e "\e[5m;1;33 ####### read dragon file  #######"
 cat dragon.txt
-echo "\e[5m;0:36 ####### Todays date  #######"
+echo -e "\e[5m;0:36 ####### Todays date  #######"
 date
