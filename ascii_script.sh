22122fef469a88329edccf69377169992301dced
vbairi29 <74100920+vbairi29@users.noreply.github.com>
2024-10-07 23:02:57 +0530

    modified script

--- ascii_script.sh
+++ ascii_script.sh
@@ -1,7 +1,13 @@
-sudo apt-get update 
+# sudo apt-get update 
+echo "####### Instaling Cowsay package #######"
 sudo apt-get install cowsay -y
+echo "####### Executing Cowsay CMD #######"
 cowsay -f dragon "I am a dragon from new city and a NEW DRAGON ...... Hee..heee...!!!" >> dragon.txt
+echo "####### Search dragon word  #######"
 grep -i "dragon" dragon.txt
+echo "####### List all files  #######"
 ls -ltra
+echo "####### read dragon file  #######"
 cat dragon.txt
+echo "####### Todays date  #######"
 date
