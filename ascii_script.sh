399ff9882fc1eefeb09994c5751dc32fe276945a
vbairi29 <74100920+vbairi29@users.noreply.github.com>
2024-10-08 12:04:26 +0530

    color

--- ascii_script.sh
+++ ascii_script.sh
@@ -2,19 +2,20 @@
 BLUE='\033[0;34m'
 WHITE= '\033[0;37m' 
 RED= '\033[0;31m'
+ORANGE= '\033[0;33m'
 #echo -e "${Blue}Welcome ${WHITE}to ${RED}France"
 # sudo apt-get update 
-echo -e "${Blue}####### Instaling Cowsay package #######"
+echo -e "${ORANGE}####### Instaling Cowsay package #######"
 sudo apt-get install cowsay -y
-echo -e "${Blue} ####### Executing Cowsay CMD #######"
+echo -e "${ORANGE}####### Executing Cowsay ${RED}CMD #######"
 cowsay -f dragon "I am a dragon from new city and a NEW DRAGON ...... Hee..heee...!!!" >> dragon.txt
-echo -e "${Blue} ####### Search dragon word  #######"
+echo -e "${ORANGE}####### Search dragon word  #######"
 grep -i "dragon" dragon.txt
-echo -e "${WHITE} ####### List all files  #######"
+echo -e "${ORANGE}####### List all files  #######"
 ls -ltra
-echo -e "${Blue} ####### read dragon file  #######"
+echo -e "${ORANGE}####### read dragon file  #######"
 cat dragon.txt
-echo -e "${RED} ####### Todays date  #######"
+echo -e "${ORANGE}####### Todays date  #######"
 date
 
 ###echo "\e[1;31mHello World!\e[0m"
